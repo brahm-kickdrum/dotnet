dd303f30f5599eb8e4721cb522e9113630e4c73f
brahm-kickdrum <brahmleen.kaur@kickdrumtech.com>
2024-05-02 23:45:46 +0530

    added files for homework-5

---
+++ Homework-5/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IntegerManipulation
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<int> numbers = Enumerable.Range(1, 10).ToList();
+
+            List<int> squaredNumbers = numbers.Select(x => x * x).ToList();
+
+            List<int> evenNumbers = numbers.Where(x => x % 2 == 0).ToList();
+
+            int sumOfEvenNumbers = evenNumbers.Sum();
+
+            Console.WriteLine("Original List:");
+            PrintList(numbers);
+
+            Console.WriteLine("\nSquared List:");
+            PrintList(squaredNumbers);
+
+            Console.WriteLine("\nFiltered List (Even Numbers Only):");
+            PrintList(evenNumbers);
+
+            Console.WriteLine("\nSum of Even Numbers: " + sumOfEvenNumbers);
+        }
+
+        static void PrintList(List<int> list)
+        {
+            foreach (int num in list)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
