7a943324793018eee1b5b8bfcafd7aab1cfeddd9
brahm <brahmleen.kaur@kickdrumtech.com>
2024-05-06 00:43:54 +0530

    Added files for homework 6

---
+++ Homework-6/Dto/StudentRequestDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Homework_6.Dto
+{
+    public class StudentRequestDto
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Grade { get; set; }
+    }
+}
